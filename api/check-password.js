9a3eae20da4d4d14f99ddba31f5e34f4eca621de
globalpowersolutionsdev <globalpowersolutionsdev@gmail.com>
2026-03-01 21:21:33 -0500

    Update check-password.js

--- api/check-password.js
+++ api/check-password.js
@@ -7,6 +7,17 @@ const VALID_CODES = {
 };
 
 export default function handler(req, res) {
+  // ALLOW ACCESS FROM OTHER SITES (CORS)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
   if (req.method !== "POST") {
     return res.status(405).end();
   }
